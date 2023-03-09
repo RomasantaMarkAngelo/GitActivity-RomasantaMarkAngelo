d0eb5d31edaffee849623156aeca3c54152f05f5
RomasantaMarkAngelo <romasantamarkangelo8@gmail.com>
2023-03-09 17:16:07 +0700

    My Personal Info Added

---
+++ git-python-romasanta.py
@@ -0,0 +1,7 @@
+college = "College of Information and Computing Sciences"
+course = "Bachelor of Science Information Technology"
+yearandsection = "2A"
+
+print (college)
+print (course)
+print (yearandsection)
