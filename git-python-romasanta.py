b05a5d4e5e3f277bf391ad0930aea810badb56c7
RomasantaMarkAngelo <romasantamarkangelo8@gmail.com>
2023-03-09 17:35:42 +0700

    My Hobbies Added

--- git-python-romasanta.py
+++ git-python-romasanta.py
@@ -13,3 +13,15 @@ yearandsection = "2A"
 print (college)
 print (course)
 print (yearandsection)
+
+hobby1 = "dancing"
+hobby2 = "singing"
+hobby3 = "eating"
+hobby4 = "playing instrument"
+hobby5 = "travel"
+
+print(hobby1)
+print(hobby2)
+print(hobby3)
+print(hobby4)
+print(hobby5 )
