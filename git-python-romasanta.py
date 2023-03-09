fa37767023a39284c9ca607c8b5791391bde16da
RomasantaMarkAngelo <romasantamarkangelo8@gmail.com>
2023-03-09 17:23:45 +0700

    My Educational Background Added

--- git-python-romasanta.py
+++ git-python-romasanta.py
@@ -5,3 +5,11 @@ yearandsection = "2A"
 print (college)
 print (course)
 print (yearandsection)
+
+college = "College of Information and Computing Sciences"
+course = "Bachelor of Science Information Technology"
+yearandsection = "2A"
+
+print (college)
+print (course)
+print (yearandsection)
